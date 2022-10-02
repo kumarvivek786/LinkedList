252397af96d80de66ed886c440032e4d017be27f
vivek <ikumarvivek786@gmail.com>
2022-10-02 23:36:24 +0530

    UC-1_CreateSimpleLinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DataStructure Linkedlist!");
+            linkedlist list = new linkedlist();
+            list.Add(30);
+            list.Add(56);
+            list.Add(70);
+            Console.Write("\nLinked List:\n");
+            list.Display();
+            Console.ReadLine(); 
+        }
+    }
+}
